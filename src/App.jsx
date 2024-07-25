759001ca6f1c285684848a3102a526a4f3032a0d
DESKTOP-RE2GR4M\gabriel <t30330@gmail.com>
2024-07-25 23:06:04 +0800

    text message

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,7 @@ function App() {
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar onStartAddProject={handleSelectedProject} />
       {content}
+      <p>delete later</p>
     </main>
   );
 }
