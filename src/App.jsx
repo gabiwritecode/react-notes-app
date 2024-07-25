b5a99180cfac21d76a6efff87fa7750e208dac0c
DESKTOP-RE2GR4M\gabriel <t30330@gmail.com>
2024-07-25 22:22:58 +0800

    initial

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import NewProject from "./components/NewProject";
+import ProjectsSidebar from "./components/ProjectsSideBar";
+import NoProjectSelected from "./components/NoProjectSelected";
+
+function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+  function handleSelectedProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+  let content;
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject></NewProject>;
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleSelectedProject} />;
+  }
+  return (
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSidebar onStartAddProject={handleSelectedProject} />
+      {content}
+    </main>
+  );
+}
+
+export default App;
